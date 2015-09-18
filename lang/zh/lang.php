f6a8e6b3bb184830340371bb441fc7b9594f56b1
Fanbo He <hefanbo@gmail.com>
2015-09-19 00:19:39 +0800

    Added Chinese translation.

---
+++ lang/zh/lang.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ */
+
+// banner
+$lang['apr_recent_draft'] = '<a href="%s">最新版本</a>尚未通过审核。';
+$lang['apr_outdated'] = '此版本<em>不是最新的</em>。有一个<a href="%s">已通过审核的较新版本</a>。';
+$lang['apr_draft'] = '此版本 (%s) 为 <em>草稿</em>。';
+$lang['apr_approved'] = '此版本 (%s) 由 %s <em> 通过审核</em>。';
+$lang['apr_previous'] = '有一个<a href="%s">较早前通过审核的版本</a> (%s)。';
+
+// Pending Approvals List
+$lang['apr_p_none'] = '没有文档需要审核。';
+$lang['apr_p_hdr_page'] = '页面';
+$lang['apr_p_hdr_previous'] = '上一次审核';
+$lang['apr_p_hdr_updated'] = '更新';
+$lang['apr_p_approved'] = '由 %s 于 <a href="%s">%s</a>';
+
+// Submit (Next to the tick-box to approve a page)
+$lang['apr_do_approve'] = '审核通过';
+
+
+$lang['wrong permissions to approve'] = '您没有权限审核此页。';
+$lang['cannot approve a non-existing revision'] = '不能审核不存在的版本。';
+$lang['version approved'] = '您已审核通过此文档。';
+$lang['cannot approve error'] = '因技术原因无法审核（权限不足？）';
+
+$lang['approvals'] = '审核情况：%s/%s';
+$lang['approved by'] = '由 %s 通过审核';
+$lang['approve action'] = '审核通过';
+
+$lang['mail_invalid'] = '在 Publish 插件中，设置了更新提醒，但输入的邮件地址无效：%s';
+
+$lang['apr_do_approve'] = '审核通过';
+$lang['apr_mail_subject'] = '新修改建议';
+$lang['apr_mail_app_subject'] = '修改建议已通过';
---
+++ lang/zh/settings.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+$lang['apr_namespaces'] = '本插件对下列命名空间有效（以空格分隔）';
+$lang['no_apr_namespaces'] = '本插件对下列命名空间<strong>无效</strong>（以空格分隔）';
+$lang['number_of_approved'] = '通过审核所需的票数';
+$lang['hidereaderbanner'] = '对只读用户隐藏审核状态';
+$lang['hide drafts'] = '对只读用户隐藏草稿';
+$lang['hide_approved_banner']  = '在已通过审核的页面中隐藏审核状态';
+$lang['author groups'] = '草稿对下列组可见（以空格分隔）';
+$lang['internal note'] = '在未通过审核的页面上显示以下内容';
+$lang['delete attic on first approve'] = '在首次通过审核时删除以往的草稿';
+
+$lang['send_mail_on_approve'] = '当有页面被审核通过时发送邮件提醒';
+$lang['apr_mail_receiver'] = '当有页面被更改时，向此地址发送提醒邮件';
+$lang['apr_approved_text'] = '用于表示“审核通过”的文本';
